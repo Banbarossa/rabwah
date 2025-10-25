c5d90be2ecd7fc72641c08f6b01588e055d93198
Banbarossa <banbarossa@gmail.com>
2025-10-26 00:10:00 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Http\Controllers\MidtranNotificationController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
-Route::post('/midtrans/notification', [MidtranNotificationController::class, 'handleNotification']);
+Route::post('/midtrans/notification', [\App\Http\Controllers\MidtranNotificationController::class, 'handleNotification']);
